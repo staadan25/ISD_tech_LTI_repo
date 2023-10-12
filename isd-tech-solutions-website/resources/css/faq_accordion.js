bb5771fc53d519986937a80ae83ef0eaebeda18b
unknown <staadan25@gibsonek.org>
2023-10-12 14:00:33 -0700

    added contact us page and tried to implement a accordion dropdown

---
+++ isd-tech-solutions-website/resources/css/faq_accordion.js
@@ -0,0 +1,5 @@
+//i have no idea what i'm doing
+function openaccordion(){
+    document.getElementsByClassName("panel").style.display="active";
+    document.getElementsByClassName("panel").style.overflow="visible";
+}
