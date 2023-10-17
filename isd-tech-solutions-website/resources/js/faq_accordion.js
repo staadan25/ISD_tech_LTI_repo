0da3f8c666b59706bdef28ceded563e861f0c610
staadan25 <staadan25@gibsonek.org>
2023-10-17 11:20:11 -0700

    extremely productive worktime

--- isd-tech-solutions-website/resources/js/faq_accordion.js
+++ isd-tech-solutions-website/resources/js/faq_accordion.js
@@ -1,5 +1,12 @@
 //i have no idea what i'm doing
 function openaccordion(){
-    document.getElementsByClassName("panel").style.display="active";
-    document.getElementsByClassName("panel").style.overflow="visible";
-}
+    var panelclass = document.getElementsByClassName("panel");
+    for (var i=0; i < panelclass.length; i++){
+            let newbutton = document.createElement("button");
+            document.getElementById("buttonhouse").appendChild(newbutton);
+            newbutton.classList.add("childbutton");
+            newbutton.textContent="MORE"
+            newbutton.setAttribute("onclick","openaccordion()")
+            newbutton.style.backgroundColor="rgb("+String(Math.random() *255)+","+String(Math.random() *255)+","+String(Math.random() *255)+")"
+        }
+    }
