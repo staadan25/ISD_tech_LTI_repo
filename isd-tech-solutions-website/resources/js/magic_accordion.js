61b649225d26e5a73dc8bce54482e819b69b10b0
staadan25 <staadan25@gibsonek.org>
2023-10-24 10:07:23 -0700

    'removed' the multiply me button

--- isd-tech-solutions-website/resources/js/magic_accordion.js
+++ isd-tech-solutions-website/resources/js/magic_accordion.js
@@ -1,5 +1,4 @@
-//i have no idea what i'm doing
-function openaccordion(){
+function openaccordion(){// this function makes a new button with a random rgb value
     var panelclass = document.getElementsByClassName("panel");
     for (var i=0; i < panelclass.length; i++){
             let newbutton = document.createElement("button");
@@ -7,10 +6,10 @@ function openaccordion(){
             newbutton.classList.add("childbutton");
             newbutton.textContent="MORE"
             newbutton.setAttribute("onclick","openaccordion()")
-            newbutton.style.backgroundColor="rgb("+String(Math.random() *255)+","+String(Math.random() *255)+","+String(Math.random() *255)+")"
+            newbutton.style.backgroundColor="rgb("+String(Math.random() *255)+","+String(Math.random() *255)+","+String(Math.random() *255)+")"//this is a wack ass way to randomize the rgb value of the button
         }
     }
-function showbutton(){
+function showbutton(){//shows the button
     var DropdownsClass = document.getElementsByClassName("dropdowns");
     for (var i=0; i < DropdownsClass.length; i++){
         DropdownsClass[i].style.display="block";
